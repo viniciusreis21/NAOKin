f875560ad7afa033092073ee2f56f22a155828a5
viniciusreis21 <vinicius.reis@engenharia.ufjf.br>
2020-06-29 12:36:59 -0300

    Excluindo parte da build-local

---
+++ build-rinobot-robot/CMakeFiles/naoKin.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viniciusreis/Projetos/NAOKin/main.cpp" "/home/viniciusreis/Projetos/NAOKin/build-rinobot-robot/CMakeFiles/naoKin.dir/main.cpp.o"
+  "/home/viniciusreis/Projetos/NAOKin/src/NAOKin.cpp" "/home/viniciusreis/Projetos/NAOKin/build-rinobot-robot/CMakeFiles/naoKin.dir/src/NAOKin.cpp.o"
+  "/home/viniciusreis/Projetos/NAOKin/src/Timer.cpp" "/home/viniciusreis/Projetos/NAOKin/build-rinobot-robot/CMakeFiles/naoKin.dir/src/Timer.cpp.o"
+  "/home/viniciusreis/Projetos/NAOKin/src/external/Kofinas/NAOKinematics.cpp" "/home/viniciusreis/Projetos/NAOKin/build-rinobot-robot/CMakeFiles/naoKin.dir/src/external/Kofinas/NAOKinematics.cpp.o"
+  "/home/viniciusreis/Projetos/NAOKin/src/external/Kofinas/NAOKinematicsJacobian.cpp" "/home/viniciusreis/Projetos/NAOKin/build-rinobot-robot/CMakeFiles/naoKin.dir/src/external/Kofinas/NAOKinematicsJacobian.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/libnaoqi/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/zlib/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/openni2/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/udev/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/usb_1/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/tbb/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/tiff/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/jpeg/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/ffmpeg/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/bzip2/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/v4l/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/png/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/gstreamer/include/gstreamer-0.10"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/glib2/include/glib-2.0"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/glib2/lib/glib-2.0/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/ffi/include"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/xml2/include/libxml2"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/boost/include/boost-1_55"
+  "/home/viniciusreis/Downloads/Mari/sdk/ctc-linux64-atom-2.1.4.13/cross/i686-aldebaran-linux-gnu/sysroot/usr/include"
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
